d95ffd1d0771bbfdcc7d0ad9827ba82e9cc8eb2d
Valc <rafaelgomezcunill@gmail.com>
2021-09-09 00:28:37 +0200

    The right modiffied file

--- administrator/components/com_localise/Field/TranslationsField.php
+++ administrator/components/com_localise/Field/TranslationsField.php
@@ -10,13 +10,13 @@ namespace Joomla\Component\Localise\Administrator\Field;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Form\Field\GroupedlistField;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\Component\Localise\Administrator\Helper\LocaliseHelper;
-use Joomla\Utilities\ArrayHelper;
 
 include_once JPATH_ADMINISTRATOR . '/components/com_localise/Helper/defines.php';
 
@@ -45,21 +45,59 @@ class TranslationsField extends GroupedlistField
 	 */
 	protected function getGroups()
 	{
+		$params         = ComponentHelper::getParams('com_localise');
+		$reftag	        = $params->get('reference');
+
+		$formdata       = $this->form->getData();
+		$langtag        = $formdata["language"];
+
+		$istranslation  = $reftag != $langtag;
+
+		$coreadminfiles = array();
+		$coresitefiles  = array();
+		$noncorefiles   = array();
+		$extrafiles     = array();
+
+		$requiredtags   = array($reftag);
+
+		if ($istranslation)
+		{
+			$requiredtags[] = $langtag;
+		}
+
 		// Remove '.ini' from values
 		if (is_array($this->value))
 		{
 			foreach ($this->value as $key => $val)
 			{
-				$this->value[$key] = substr($val, 0, -4);
+				$ext = File::getExt($val);
+
+				if ($ext == "ini")
+				{
+					$this->value[$key] = substr($val, 0, -4);
+				}
 			}
 		}
+
+		$xml            = simplexml_load_file(JPATH_ROOT . '/media/com_localise/packages/core.xml');
+		$coreadminfiles = (array) $xml->administrator->children();
+		$coresitefiles  = (array) $xml->site->children();
+
+		$coresitefiles  = $coresitefiles['filename'];
+		$coreadminfiles = $coreadminfiles['filename'];
+
+		$coreadminfiles = self::suffix_array_values($coreadminfiles, '.ini');
+		$coresitefiles  = self::suffix_array_values($coresitefiles, '.ini');
 
 		$package = (string) $this->element['package'];
 		$groups  = array('Site' => array(), 'Administrator' => array(), 'Installation' => array());
 
 		foreach (array('Site', 'Administrator', 'Installation') as $client)
 		{
-			$path = constant('LOCALISEPATH_' . strtoupper($client)) . '/language';
+			$noncorefiles[$client] = array();
+			$extrafiles[$client]   = array();
+
+			$path                  = constant('LOCALISEPATH_' . strtoupper($client)) . '/language';
 
 			if (Folder::exists($path))
 			{
@@ -69,10 +107,48 @@ class TranslationsField extends GroupedlistField
 				{
 					foreach ($tags as $tag)
 					{
+						if (!in_array($tag, $requiredtags))
+						{
+							continue;
+						}
+
 						$files = Folder::files("$path/$tag", ".ini$");
+
+						if ($client == 'site' && $tag == $reftag)
+						{
+							$noncorefiles[$client] = array_diff($files, $coresitefiles);
+						}
+						elseif ($client == 'Administrator' && $tag == $reftag)
+						{
+							$noncorefiles[$client] = array_diff($files, $coreadminfiles);
+						}
+
+						if ($istranslation && $client == 'site' && $tag == $langtag)
+						{
+							$extrafiles[$client] = array_diff($files, $coresitefiles);
+						}
+						elseif ($istranslation && $client == 'Administrator' && $tag == $langtag)
+						{
+							$extrafiles[$client] = array_diff($files, $coreadminfiles);
+						}
 
 						foreach ($files as $file)
 						{
+							$class = '';
+
+							if (in_array($file, $noncorefiles[$client]))
+							{
+								$class = 'not-in-core';
+							}
+							elseif (in_array($file, $extrafiles[$client]))
+							{
+								$class = 'extra-in-translation';
+							}
+							else
+							{
+								$class = 'core-file';
+							}
+
 							if ($file == 'joomla.ini')
 							{
 								$key      = 'joomla';
@@ -88,49 +164,11 @@ class TranslationsField extends GroupedlistField
 								$disabled = $origin != $package && $origin != '_thirdparty';
 							}
 
-							$groups[$client][$key] = HTMLHelper::_('select.option', strtolower($client) . '_' . $key, $value, 'value', 'text', false);
-						}
-					}
-				}
-			}
-		}
-
-		$scans = LocaliseHelper::getScans();
-
-		foreach ($scans as $scan)
-		{
-			$prefix     = $scan['prefix'];
-			$suffix     = $scan['suffix'];
-			$type       = $scan['type'];
-			$client     = ucfirst($scan['client']);
-			$path       = $scan['path'];
-			$folder     = $scan['folder'];
-			$extensions = Folder::folders($path);
-
-			foreach ($extensions as $extension)
-			{
-				if (Folder::exists("$path$extension$folder/language"))
-				{
-					// Scan extensions folder
-					$tags = Folder::folders("$path$extension$folder/language");
-
-					foreach ($tags as $tag)
-					{
-						/* @ Todo: Deal with extensions using old format*/
-						$file = "$path$extension$folder/language/$tag/$prefix$extension$suffix.ini";
-
-						if (File::exists($file))
-						{
-							$origin   = LocaliseHelper::getOrigin("$prefix$extension$suffix", strtolower($client));
-							$disabled = $origin != $package && $origin != '_thirdparty';
-
-							/* @ Todo: $disabled prevents choosing some core files when creating package.
-							 $groups[$client]["$prefix$extension$suffix"] = HTMLHelper::_(
-							'select.option', strtolower($client) . '_' . "$prefix$extension$suffix", "$prefix$extension$suffix", 'value', 'text', $disabled);
-							*/
-							$groups[$client]["$prefix$extension$suffix"] = HTMLHelper::_(
-									'select.option', strtolower($client) . '_' . "$prefix$extension$suffix", "$prefix$extension$suffix", 'value', 'text', false
-							);
+							if (!array_key_exists($key, $groups[$client]))
+							{
+								$groups[$client][$key] = HTMLHelper::_('select.option', strtolower($client) . '_' . $key, $value, 'value', 'text', false);
+								$groups[$client][$key]->class = $class;
+							}
 						}
 					}
 				}
@@ -146,5 +184,34 @@ class TranslationsField extends GroupedlistField
 		$groups = array_merge(parent::getGroups(), $groups);
 
 		return $groups;
+	}
+
+	/**
+	 * Method to add a suffix to an array.
+	 *
+	 * @param   array   $array   An array of core files.
+	 * @param   string  $suffix  The suffix to add to each file.
+	 *
+	 * @return  array   The modified array
+	 */
+	public static function suffix_array_values($array, $suffix = '')
+	{
+		if (!is_array($array))
+		{
+			return false;
+		}
+
+		// Suffix the values and respect the keys
+		foreach ($array as $key => $value)
+		{
+			if (!is_string($value))
+			{
+				continue;
+			}
+
+			$array[$key] = $value . $suffix;
+		}
+
+		return $array;
 	}
 }
