3ba7b206f175d0cf90fb8ba981e2339fedcc253c
Valc <rafaelgomezcunill@gmail.com>
2021-10-05 19:06:25 +0200

    Solve issue with extras value

--- administrator/components/com_localise/View/Translation/HtmlView.php
+++ administrator/components/com_localise/View/Translation/HtmlView.php
@@ -81,12 +81,7 @@ class HtmlView extends BaseHtmlView
 		$checkedOut	   = !($this->item->checked_out == 0 || $this->item->checked_out == $user->get('id'));
 		$complete      = (int) ComponentHelper::getParams('com_localise')->get('complete', 0);
 		$istranslation = $this->get('Item')->istranslation;
-		$has_notinref  = false;
-
-		if (!empty($this->get('Item')->developdata))
-		{
-			$has_notinref = ($this->get('Item')->developdata['extra_keys']['amount'] > 0 ? true : false);
-		}
+		$has_notinref  = ($this->get('Item')->extra > 0) ? true : false;
 
 		$toolbar = Toolbar::getInstance('toolbar');
 
@@ -114,8 +109,7 @@ class HtmlView extends BaseHtmlView
 
 				if ($has_notinref && $istranslation)
 				{
-					$message .= '\\n'
-					. Text::_('COM_LOCALISE_CONFIRM_TRANSLATION_SAVE_NOTINREF');
+					$message .= '\n\n' . Text::_('COM_LOCALISE_CONFIRM_TRANSLATION_SAVE_NOTINREF');
 				}
 
 				$toolbar->confirmButton('apply')
